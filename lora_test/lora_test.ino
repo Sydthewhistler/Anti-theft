44eb25657609d07f25ebe8dff47e5be95686600e
Sydney Cavallin <cavallin.sydney@gmail.com>
2026-05-22 13:52:14 +0200

    Fix: lora

--- lora_test/lora_test.ino
+++ lora_test/lora_test.ino
@@ -1,56 +1,85 @@
-#include <RadioLib.h>
+#define CFG_EU 1
 
-RFM95 radio = new Module(10, 6, 8, -1);
+#include <lmic.h>
+#include <hal/hal.h>
+#include <SPI.h>
 
-uint32_t devAddr = 0x260B9C2F;
+static const u4_t DEVADDR = 0x260B9C2F;
 
-uint8_t nwkSKey[] = {
+static const PROGMEM u1_t NWKSKEY[16] = {
   0x84, 0x86, 0xC7, 0x16, 0x7A, 0x66, 0x6D, 0x95,
   0x3B, 0x8A, 0xE5, 0x66, 0x16, 0x63, 0xC9, 0xF4
 };
 
-uint8_t appSKey[] = {
+static const u1_t PROGMEM APPSKEY[16] = {
   0x01, 0xAB, 0xC5, 0x9D, 0x2E, 0xE5, 0x47, 0x2F,
   0xE6, 0x83, 0xC8, 0x0D, 0x54, 0x54, 0xA5, 0x39
 };
 
-LoRaWANNode node(&radio, &EU868);
+void os_getArtEui(u1_t* buf) {}
+void os_getDevEui(u1_t* buf) {}
+void os_getDevKey(u1_t* buf) {}
+
+static uint8_t mydata[] = "ALARME";
+static osjob_t sendjob;
+const unsigned TX_INTERVAL = 30;
+
+const lmic_pinmap lmic_pins = {
+  .nss  = 10,
+  .rxtx = LMIC_UNUSED_PIN,
+  .rst  = 8,
+  .dio  = {6, 6, 6},
+};
+
+void onEvent(ev_t ev) {
+  switch (ev) {
+    case EV_TXCOMPLETE:
+      Serial.println(F("TX complete !"));
+      os_setTimedCallback(&sendjob, os_getTime() + sec2osticks(TX_INTERVAL), do_send);
+      break;
+    default:
+      Serial.println(F("Event inconnu"));
+      break;
+  }
+}
+
+void do_send(osjob_t* j) {
+  if (LMIC.opmode & OP_TXRXPEND) {
+    Serial.println(F("TX en cours..."));
+  } else {
+    LMIC_setTxData2(1, mydata, sizeof(mydata) - 1, 0);
+    Serial.println(F("Packet queued"));
+  }
+}
 
 void setup() {
   Serial.begin(115200);
   delay(1000);
-  Serial.println("Starting...");
+  Serial.println(F("Starting..."));
 
-  int state = radio.begin(868.1);
-  if (state != RADIOLIB_ERR_NONE) {
-    Serial.print("Radio init failed: ");
-    Serial.println(state);
-    while (1);
-  }
-  Serial.println("Radio OK");
+  os_init();
+  LMIC_reset();
+  LMIC_setClockError(MAX_CLOCK_ERROR * 2 / 100);
 
-  state = node.beginABP(devAddr, nwkSKey, nwkSKey, nwkSKey, appSKey);
-  if (state != RADIOLIB_ERR_NONE) {
-    Serial.print("LoRaWAN init failed: ");
-    Serial.println(state);
-    while (1);
-  }
-  Serial.println("LoRaWAN OK");
+  uint8_t appskey[sizeof(APPSKEY)];
+  uint8_t nwkskey[sizeof(NWKSKEY)];
+  memcpy_P(appskey, APPSKEY, sizeof(APPSKEY));
+  memcpy_P(nwkskey, NWKSKEY, sizeof(NWKSKEY));
+  LMIC_setSession(0x1, DEVADDR, nwkskey, appskey);
+
+  #if defined(CFG_EU)
+  LMIC_setupChannel(0, 868100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);
+  LMIC_setupChannel(1, 868300000, DR_RANGE_MAP(DR_SF12, DR_SF7B), BAND_CENTI);
+  LMIC_setupChannel(2, 868500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);
+  #endif
+
+  LMIC_setLinkCheckMode(0);
+  LMIC.dn2Dr = DR_SF9;
+  LMIC_setDrTxpow(DR_SF9, 20);
+
+  do_send(&sendjob);
 }
 
 void loop() {
-  uint8_t payload[] = { 0x01, 0x00, 0x01 };
-
-  Serial.println("Envoi...");
-  int state = node.sendReceive(payload, sizeof(payload), 1);
-
-  if (state == RADIOLIB_ERR_NONE) {
-    Serial.println("OK !");
-  } else {
-    Serial.print("Erreur: ");
-    Serial.println(state);
-  }
-
-  Serial.println("Attente 30s...");
-  delay(30000);
+  os_runloop_once();
 }
